6e134ebf210f0bb1f2317a4b021dbc46a36e5ff1
Anden <73430931+AndreasEstmark@users.noreply.github.com>
2025-08-17 21:40:24 +0200

    small

--- src/regression_analysis/utils/helpers.py
+++ src/regression_analysis/utils/helpers.py
@@ -25,3 +25,11 @@ def impute_missing_values(X:np.ndarray, imputation_method: str = "mean")->np.nda
     Method to impute missing values in the data.
     """
     pass
+
+
+def standardize_data(X: np.ndarray) -> np.ndarray:
+    """
+    Method to standardize the data.
+    This is useful for linear regression models when the features are on different scales.
+    """
+    pass
